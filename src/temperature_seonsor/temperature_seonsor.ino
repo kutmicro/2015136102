6e8083e86b708c7f22719ed84e897428d763ac2b
2013136082_R-pet'sSafeHabitat <kutmicro@gmail.com>
2016-11-01 17:18:10 +0900

    소스

---
+++ src/temperature_seonsor/temperature_seonsor.ino
@@ -0,0 +1,20 @@
+#include <math.h>
+int a;
+float temperature;
+int B=3975;                  //B value of the thermistor
+float resistance;
+
+void setup()
+{
+  Serial.begin(9600);  
+}
+
+void loop()
+{
+  a=analogRead(0);
+  resistance=(float)(1023-a)*10000/a; //get the resistance of the sensor;
+  temperature=1/(log(resistance/10000)/B+1/298.15)-273.15;//convert to temperature via datasheet ;
+  delay(1000);
+  Serial.print("Current temperature is ");
+  Serial.println(temperature);
+ }
